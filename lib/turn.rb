a335b4285ca34a747bce87359af998c6c55e75ca
ikenna agulobi <ikennaww@yahoo.fr>
2020-05-01 20:09:21 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def move(board,input_to_index,move="X")
  def turn(board)
     puts "Please enter 1-9:"
        user_input = gets.chomp
-         input_to_index(ike)
+        index_converted = input_to_index(user_input)
       
    
 end
